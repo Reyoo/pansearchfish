8d5d5f4d27d1de8226062a1b2785c39439f5bf83
huangshuai <shuai.huang.bj@nikoyo.com.cn>
2021-02-26 16:06:37 +0800

    2-26 1.小悠爬虫优化

--- src/main/java/top/findfish/crawler/common/CrawlerScheduleTask.java
+++ src/main/java/top/findfish/crawler/common/CrawlerScheduleTask.java
@@ -58,7 +58,7 @@ public class CrawlerScheduleTask {
      */
     //3.添加定时任务  双数小时  2，4，6，8，10...
 //    @Scheduled(cron = "0 30 0/2 * * ? ")
-    @Scheduled(cron = "0 30 0/2 * * ? ")
+    @Scheduled(cron = "0 11 0/2 * * ? ")
 
     //或直接指定时间间隔，例如：5秒
 //    @Scheduled(fixedRate=5000)
