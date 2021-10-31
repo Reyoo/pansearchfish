f7c8423a88a4d642dba5bdac4a11572ef0e6cf98
SunQi <sun7127@126.com>
2021-10-31 15:09:31 +0800

    Redis 去除无用换行code

--- src/main/java/top/findfish/crawler/common/RedisConfig.java
+++ src/main/java/top/findfish/crawler/common/RedisConfig.java
@@ -33,50 +33,43 @@ import java.time.Duration;
 
 @Configuration
 @EnableCaching
-public class RedisConfig extends CachingConfigurerSupport
-{
+public class RedisConfig extends CachingConfigurerSupport {
     @Bean
     @Override
-    public KeyGenerator keyGenerator()
-    {
-        return (o,method,objects)->
+    public KeyGenerator keyGenerator() {
+        return (o, method, objects) ->
         {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append(o.getClass().getName());
             stringBuilder.append(method.getName());
-            for (Object obj : objects)
-            {
+            for (Object obj : objects) {
                 stringBuilder.append(obj.toString());
             }
             return stringBuilder.toString();
         };
     }
+
     /**
-     *
      * Description: redisTemplate序列化
      * @param factory
      * @author huangweicheng
      * @date 2019/10/22
      */
     @Bean
-    public RedisTemplate redisTemplate(RedisConnectionFactory factory)
-    {
+    public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate redisTemplate = new RedisTemplate<Object, Object>();
         redisTemplate.setConnectionFactory(factory);
-
         Jackson2JsonRedisSerializer<Object> j2jrs = new Jackson2JsonRedisSerializer<>(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         // 解决jackson2无法反序列化LocalDateTime的问题
         om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         om.registerModule(new JavaTimeModule());
-        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance ,ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
         j2jrs.setObjectMapper(om);
         // 序列化 value 时使用此序列化方法
         redisTemplate.setValueSerializer(j2jrs);
         redisTemplate.setHashValueSerializer(j2jrs);
-
 
         /*设置key的序列化*/
         redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -91,14 +84,12 @@ public class RedisConfig extends CachingConfigurerSupport
     public CacheManager cacheManager(RedisConnectionFactory factory) {
         RedisSerializer<String> redisSerializer = new StringRedisSerializer();
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-
         //解决查询缓存转换异常的问题
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance ,ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
 //        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
-
         // 配置序列化（解决乱码的问题）
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofHours(3L))
