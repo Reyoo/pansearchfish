c856e8626b31781cc7c81a62631c29b96b6d7896
黄帅 <shuai.huang.bj@nikoyo.com.cn>
2022-06-14 14:55:26 +0800

    2022-06-14 1.新增 小宇网站 替代 莉莉网站

--- findfish-wx/src/main/java/com/libbytian/pan/system/controller/CommonManagerController.java
+++ findfish-wx/src/main/java/com/libbytian/pan/system/controller/CommonManagerController.java
@@ -5,7 +5,10 @@ import com.libbytian.pan.system.service.ISystemUserSearchMovieService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -29,10 +32,13 @@ public class CommonManagerController {
      * @param date 0为当日，7为近7天，30为近30天
      * @return
      */
-    @RequestMapping(value = "/select/{date}", method = RequestMethod.GET)
-    public AjaxResult getHotList(@PathVariable Integer date, @RequestParam(defaultValue = "1") Integer pageNum , @RequestParam(defaultValue = "10") Integer pageSize) {
+    @RequestMapping(value = "/select/hotList", method = RequestMethod.GET)
+    public AjaxResult getHotList(@RequestParam(defaultValue = "1") Integer date,
+                                 @RequestParam(defaultValue = "1") Integer pageNum , @RequestParam(defaultValue = "10") Integer pageSize) {
         Map<String,Object> hotList = iSystemUserSearchMovieService.getFalseDataHotList(date,pageNum,pageSize);
         return AjaxResult.success(hotList);
 
     }
+
+
 }
