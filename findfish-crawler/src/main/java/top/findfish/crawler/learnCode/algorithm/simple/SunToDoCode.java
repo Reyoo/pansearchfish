5b6cdeb23a92f9990aa7c4cd63b58f0afbe26129
SunQi <sun7127@126.com>
2022-10-14 21:48:05 +0800

    devp:新增无效代码，新增测试

---
+++ findfish-crawler/src/main/java/top/findfish/crawler/learnCode/algorithm/simple/SunToDoCode.java
@@ -0,0 +1,21 @@
+package top.findfish.crawler.learnCode.algorithm.simple;
+
+/**
+ * *************************************************************************
+ * <p/>
+ *
+ * @文件名称: SunToDoCode.java
+ * @包 路 径： top.findfish.crawler.learnCode.algorithm.simple
+ * @版权所有：北京数字认证股份有限公司 (C) 2021
+ * @类描述:
+ * @版本: V1.0
+ * @Author：SunQi
+ * @创建时间：2022/10/14 21:46
+ */
+public class SunToDoCode {
+
+    public static void main(String[] args) {
+        // c语言学习
+        System.out.println("learn c ~");
+    }
+}
--- findfish-wx/src/test/java/com/libbytian/pan/SearchpanApplicationTests.java
+++ findfish-wx/src/test/java/com/libbytian/pan/SearchpanApplicationTests.java
@@ -8,7 +8,9 @@ import com.libbytian.pan.findmovie.unread.IFindMovieInUnread;
 import com.libbytian.pan.findmovie.xiaoyou.IFindMovieInXiaoyou;
 import com.libbytian.pan.findmovie.youjiang.IFindMovieInYoujiang;
 import com.libbytian.pan.system.model.SystemTemDetailsModel;
+import com.libbytian.pan.system.model.SystemUserToRole;
 import com.libbytian.pan.system.service.ISystemTemDetailsService;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.UUID;
 import java.util.regex.Pattern;
 
 @SpringBootTest
@@ -88,6 +91,110 @@ class SearchpanApplicationTests {
         BigDecimal divide1 = bigDecimal.divide(bigDecimal1, RoundingMode.UP);
         System.out.println(divide.intValue());
         System.out.println(divide1.intValue());
+    }
+
+
+    @Test
+   void testaaa(){
+//        SystemUserToRole systemUserToRole = new SystemUserToRole();
+//        systemUserToRole.setRoleId("1");
+//        systemUserToRole.setUserId("1");
+//        systemUserToRole.setUserToRoleId("1");
+//        systemUserToRole.setUserRoleStatus(true);
+//        systemUserToRole.setChecked(true);
+//        ThreadLocalTest.set(systemUserToRole);
+
+//        for(int i=0;i<=10;i++){
+//
+//            Thread thread = new Thread(() ->{
+//                SystemUserToRole systemUserToRole = new SystemUserToRole();
+//                systemUserToRole.setRoleId("1");
+//                systemUserToRole.setUserId("1");
+//                systemUserToRole.setUserToRoleId("1");
+//                systemUserToRole.setUserRoleStatus(true);
+//                systemUserToRole.setChecked(true);
+//                ThreadLocalTest.set(systemUserToRole);
+//                SystemUserToRole s = new SystemUserToRole();
+//                s.setRoleId("2");
+//                s.setUserId("2");
+//                s.setUserToRoleId("2");
+//                s.setUserRoleStatus(false);
+//                s.setChecked(false);
+//                ThreadLocalTest.set(s);
+//
+//                System.out.println(Thread.currentThread().getName()+"  " +Thread.currentThread().getId() + " "+ThreadLocalTest.get());
+//            });
+//            thread.start();
+//        }
+
+//        System.out.println(generateValue());
+
+//        upgradeCertChannel("0000000000","L31");
+
+        String l2 = "l2";
+        String l3 = "l3";
+        String l31 = "l31";
+        String l1 = "l1";
+
+//        System.out.println(l1.compareTo(l2));
+//        System.out.println(l2.compareTo(l2));
+//        System.out.println(l3.compareTo(l2));
+//
+//        System.out.println(l2.compareTo(l1));
+//        System.out.println(l2.compareTo(l2));
+//        System.out.println(l2.compareTo(l3));
+
+        System.out.println(l3.compareTo(l31));
+
+
+
+
+//        String name = "bjt_13522215221";
+//        System.out.println(name.length());
+//        System.out.println(name+2);
+
+
+        System.out.println(stest());
+
+    }
+
+    public static boolean stest(){
+
+        String l2 = "l2";
+        String l3 = "l3";
+        String l31 = "l31";
+        String l1 = "l1";
+        System.out.println(l3.compareTo(l31));
+        return l3.compareTo(l31) >0;
+    }
+
+
+
+
+    public static String upgradeCertChannel(String userCertChannel, String certLevel) {
+        if (StringUtils.isBlank(userCertChannel)) {
+            userCertChannel = "0000000000";
+        }
+        StringBuilder sb = new StringBuilder(userCertChannel);
+        sb.replace(0, 1, "1");
+        if (StringUtils.equals("L31", certLevel)) {
+            sb.replace(1, 2, "1");
+        } else if (StringUtils.equals("L32", certLevel)) {
+            sb.replace(2, 3, "1");
+        }
+        return sb.toString();
+    }
+
+
+
+    public static String generateValue() {
+        return generateValue(UUID.randomUUID().toString());
+    }
+
+    public static String generateValue(String param) {
+        String s = UUID.fromString(UUID.nameUUIDFromBytes(param.getBytes()).toString()).toString();
+        String replace = s.replace("-", "");
+        return "new_".concat(replace);
     }
 }
 
